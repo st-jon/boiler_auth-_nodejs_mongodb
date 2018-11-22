2e5c2fc12e190cabf507766cd61f4b2d0607a36a
christophe <c.johanny@gmail.com>
2018-11-23 00:22:28 +0100

    some stuff done more to come

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
+let bodyParser = require("body-parser")
+let exphbs = require('express-handlebars')
+let express = require('express')
+let flash = require('connect-flash')
+let LocalStrategy = require('passport-local').Strategy
+let mongoose = require('mongoose')
+let passport = require('passport')
+let session = require('express-session')
+let validator = require('validator')
 
-require('./models/User')
-let UserSchema = require('mongoose').model('User').schema
+let {User} = require('./models/User')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+app.use(session({
+  secret: 'fdkjhsfUYTIUYbjdf',
+  resave: false,
+  saveUninitialized: true,
+}))
 
-app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+app.engine('.hbs', exphbs({defaultLayout: 'main'}))
+app.set('view engine', '.hbs')
+
+mongoose.connect('mongodb://localhost/user', { useNewUrlParser: true })
+mongoose.set('useCreateIndex', true)
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -16,15 +35,102 @@ db.once('open', function() {
   console.log('connected to mongoDB')
 })
 
-const Users = mongoose.model('User', UserSchema)
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({$or: [{ username: username }, {email: username}]}, (err, user) => {
+      if (err) {
+        console.log(err)
+        res.sendStatus(500)
+        return
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' })
+      }
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) {
+          console.log(err)
+          res.sendStatus(500)
+          return
+        }
+        if (isMatch) {
+          return done(null, user)
+        } else {
+          return done(null, false, { message: 'Invalid password' })
+        }
+      })
+    })
+  }
+))
 
-let christophe = new Users({ firstname: 'christophe' })
-console.log(christophe.firstname)
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
 })
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user)
+  })
+})
+
+app.get('/', (req, res) => {
+  res.render('index', {success: req.flash('success')})
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', {error: req.flash('error'), message: req.flash('message')})
+})
+
+app.post('/login', passport.authenticate('local', { 
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+  successFlash: true
+  })
+)
+
+app.get('/register', (req, res) => {
+  res.render('register', {message: req.flash('message')})
+})
+
+app.post('/register', (req, res) => {
+  let username= req.body.username
+  let firstname = req.body.firstname
+  let lastname = req.body.lastname
+	let email = req.body.email
+	let password = req.body.password
+  let confirm = req.body.confirm
+
+  let newUser = new User({
+    username: username,
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    password: password
+  })
+  
+  
+  User.createUser(newUser, (err, user) => {
+    if(user) {
+      req.flash('message', `Hi ${user.username}! You are registered`)
+      res.redirect('/login')
+    }
+    if (err) {
+      if(err.code === 11000) {
+        req.flash('message', 'Username already used')
+        res.redirect('/register')
+      } 
+    }
+    
+  })
+})
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+app.set('port', (process.env.PORT || 3000))
+
+app.listen(app.get('port'), () => {
+	console.log(`Server started on port ${app.get('port')}`)
 })
--- models/User.js
+++ models/User.js
@@ -1,11 +1,66 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 let UserSchema = new mongoose.Schema({
-  firstname:String, 
-  lastname:String,
-  email:String,  
-  password:String,
+  firstname: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  lastname:{
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 1,
+    unique: true,
+    trim: true
+  },
+  email:{
+    type: String,
+    required: true,
+    minlength: 1,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE}, is not a valid email."
+    }
+  }, 
+  password:{
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 })
 
+UserSchema.statics.createUser = function (newUser, callback) {
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash
+	        newUser.save(callback)
+	    })
+	})
+}
 
-module.exports = mongoose.model("User",UserSchema)
+UserSchema.statics.comparePassword = function (candidatePassword, hash, callback) {
+	bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(500)
+      return
+    }
+    	callback(null, isMatch)
+	})
+}
+
+
+let User = mongoose.model("User", UserSchema)
+
+module.exports = { User }
